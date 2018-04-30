55618f9ba2108842c467e5511ca904393c4df645
Tommy Tang <tomisme25@me.com>
2018-04-30 16:59:11 -0700

    Toggled some more shit

---
+++ src/MaxGapInArray.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class MaxGapInArray {
+	public static void main(String[] args) {
+	
+
+	}
+
+	public int maxGap(int[] arr) {
+		if (arr == null || arr.length < 2) return 0;
+
+		// Scan once to get the min and max value
+		int len = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+		for (int num : arr) {
+			min = Math.min(min, num);
+			max = Math.max(max, num);
+		}
+
+		// Build n-1 buckets based on the min and max value, actually, just need something to record the min/max of buckets
+		int[][] buckets = new int[2][len-1];
+		buckets[0] = new int[len-1];
+		buckets[1] = new int[len-1];
+
+		Arrays.fill(buckets[0], Integer.MAX_VALUE);
+		Arrays.fill(buckets[1], Integer.MIN_VALUE);
+
+		buckets[0][0] = min;
+		buckets[1][len-2] = max;
+
+		for (int num : arr) {
+			int bucketIndex = (num - min) * (len - 1) / (max - min);
+			buckets[0][bucketIndex] = Math.min(num, buckets[0][bucketIndex]);
+			buckets[1][bucketIndex] = Math.max(num, buckets[1][bucketIndex]);
+		}
+
+		int empty = 0;
+		for (; empty < len - 1; empty++) {
+			if (buckets[0][empty] == Integer.MAX_VALUE && buckets[1][empty] == Integer.MIN_VALUE) break; 
+		}
+
+		// Since empty is assured to be in the middle instead of head or tail bucket
+		return buckets[0][empty + 1] - buckets[1][empty - 1];
+	}
+}
